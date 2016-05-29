c8da682bd673bbcc5798b94fb7960ca9e71a143f
hua dong <huadong@huas-iMac.local>
2016-05-29 15:39:51 -0700

    init commit add gitignore podfile

---
+++ AccountTask/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AccountTask
+//
+//  Created by hua dong on 2016-05-29.
+//  Copyright © 2016 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
